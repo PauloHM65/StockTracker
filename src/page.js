8932c4653abfe8041fca510802511a34310b876a
PauloHM65 <pauloharaujo345@gmail.com>
2023-12-05 14:10:36 -0300

    D

--- src/page.js
+++ src/page.js
@@ -88,10 +88,9 @@ function imprimeDado() {
 function HOME() {
     window.location.href = "http://127.0.0.1:5501/HOME/home.html";
 }
-function invent() {
+function INVENTARIO() {
     window.location.href = "http://127.0.0.1:5501/tela_categorais/tela_categorias.html";
 }
 
-document.getElementById('btnIncluirContato').addEventListener('click', incluirDados);
 document.getElementById('btncadastar').addEventListener('click', HOME);
-document.getElementById('btninventario').addEventListener('click', invent);
+document.getElementById('btninventario').addEventListener('click', INVENTARIO);
